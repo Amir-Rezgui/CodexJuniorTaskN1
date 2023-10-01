f0757652087ed5a6ac6ea79afefa83cb57d3ef94
Amir Rezgui <amir.benrezgui@etudiant-isi.utm.tn>
2023-10-01 03:05:35 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -11,7 +11,6 @@ CREATE TABLE IF NOT EXISTS users (
     reset_token_expiry DATETIME
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Ensure that the email and username columns are case-insensitive
 ALTER TABLE users
 MODIFY email VARCHAR(255) COLLATE utf8mb4_general_ci,
 MODIFY username VARCHAR(255) COLLATE utf8mb4_general_ci;
